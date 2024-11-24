4dafe4d7274bc0e84913e0e443a18b6784bf4c91
Josh Kwak <joshkwak01@gmail.com>
2024-11-24 00:59:02 -0800

    adding photo metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ const bodoniModa = Bodoni_Moda({
 export const metadata: Metadata = {
   title: "Joshua Kwak | Engineer: Software & Mechanical",
   description: "Portfolio of Joshua Kwak, Software and Mechanical Engineer",
+  openGraph: {
+    title: "Joshua Kwak | Engineer: Software & Mechanical",
+    description: "Portfolio of Joshua Kwak, showcasing software and mechanical projects.",
+    images: [
+      {
+        url: "/images/projects/portfolio.png",
+        width: 1500,
+        height: 800,
+        alt: "Joshua Kwak Portfolio",
+      },
+    ],
+    type: "website",
+    url: "https://www.joshkwak.com",
+  },
 };
 
 export default function RootLayout({
@@ -25,16 +39,21 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon.png" />
-
+        <meta property="og:title" content="Joshua Kwak | Engineer: Software & Mechanical" />
+        <meta
+          property="og:description"
+          content="Portfolio of Joshua Kwak, showcasing software and mechanical projects."
+        />
+        <meta property="og:image" content="/images/og-image.jpg" />
+        <meta property="og:url" content="https://your-portfolio-domain.com" />
+        <meta property="og:type" content="website" />
         <title>Joshua Kwak | Engineer: Software & Mechanical</title>
         <meta
           name="description"
           content="Portfolio of Joshua Kwak, Software and Mechanical Engineer"
         />
       </head>
-      <body className="font-serif">
-        {children}
-      </body>
+      <body className="font-serif">{children}</body>
     </html>
   );
 }
