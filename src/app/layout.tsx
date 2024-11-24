a568b1aa9f63f0b6baac83519e9e9a1389a203f6
Josh Kwak <joshkwak01@gmail.com>
2024-11-24 01:05:21 -0800

    just trying smt out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import "@/app/globals.css";
 import { Bodoni_Moda } from "next/font/google";
 
@@ -7,25 +6,6 @@ const bodoniModa = Bodoni_Moda({
   weight: ["400", "700"],
   variable: "--font-bodoni-moda",
 });
-
-export const metadata: Metadata = {
-  title: "Joshua Kwak | Engineer: Software & Mechanical",
-  description: "Portfolio of Joshua Kwak, Software and Mechanical Engineer",
-  openGraph: {
-    title: "Joshua Kwak | Engineer: Software & Mechanical",
-    description: "Portfolio of Joshua Kwak, showcasing software and mechanical projects.",
-    images: [
-      {
-        url: "/images/projects/portfolio.png",
-        width: 1500,
-        height: 800,
-        alt: "Joshua Kwak Portfolio",
-      },
-    ],
-    type: "website",
-    url: "https://www.joshkwak.com",
-  },
-};
 
 export default function RootLayout({
   children,
@@ -39,6 +19,19 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon.png" />
+        <meta property="og:title" content="Joshua Kwak | Engineer: Software & Mechanical" />
+        <meta
+          property="og:description"
+          content="Portfolio of Joshua Kwak, showcasing software and mechanical projects."
+        />
+        <meta property="og:image" content="/images/projects/portfolio.png" />
+        <meta property="og:url" content="https://www.joshkwak.com" />
+        <meta property="og:type" content="website" />
+        <title>Joshua Kwak | Engineer: Software & Mechanical</title>
+        <meta
+          name="description"
+          content="Portfolio of Joshua Kwak, Software and Mechanical Engineer"
+        />
       </head>
       <body className="font-serif">{children}</body>
     </html>
