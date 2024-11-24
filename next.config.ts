39f757ff595c41006a7e882f62a714d9206655b6
Josh Kwak <joshkwak01@gmail.com>
2024-11-23 21:31:14 -0800

    deploy site?

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
+const isProd = process.env.NODE_ENV === "production";
 import type { NextConfig } from "next";
+
+const repoName = "joshkwka.github.io";
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   reactStrictMode: true,
-  images: {unoptimized: true},
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
